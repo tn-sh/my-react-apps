11ebabab00d8b531e38211b6181955697a63d765
kemokemo <t2wonderland@gmail.com>
2022-01-04 23:51:18 +0900

    chore: add some test and change web vital output

--- generated-app/src/App.test.js
+++ generated-app/src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/React link/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders edit source', () => {
+  render(<App />);
+  const pElement = screen.getByText(/src\/App.js/i);
+  expect(pElement).toBeInTheDocument();
 });
