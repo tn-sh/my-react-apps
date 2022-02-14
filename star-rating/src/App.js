e8c6c39f28d37a83691854bae445cb2db01a92aa
tn-sh <88361184+tn-sh@users.noreply.github.com>
2022-02-14 15:25:18 +0900

    feat: add style property

--- star-rating/src/App.js
+++ star-rating/src/App.js
@@ -2,5 +2,7 @@ import React from "react";
 import StarRating from './StarRating';
 
 export default function App() {
-  return <StarRating />;
+  return <StarRating 
+    style={{ backgroundColor: "lightblue" }}
+  />;
 }
